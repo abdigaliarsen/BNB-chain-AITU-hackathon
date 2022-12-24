b6458a6207415372ba9421856d37e68eb5a1daff
Almaz <63015341+Morr231@users.noreply.github.com>
2022-12-24 14:27:52 +0600

    init client

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from "./UI";
+
+function App() {
+    return (
+        <div className="App">
+            Hello world
+            <Button text="Hello" />
+        </div>
+    );
+}
+
+export default App;
